220c3ee69850595e4d018e64614e11047d8b1afa
mcvollenweider <mcvollenweider@gmail.com>
2021-12-06 18:02:17 -0500

    Login and Register

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,93 @@
 // Christian is doing login
+import React, { useState, useEffect } from "react";
+import { Form, Button, Col, ButtonToolbar, FormGroup } from "react-bootstrap";
+import { loginUser } from "../api";
+import { storeToken, storeUser} from "../auth"
+import { useHistory } from "react-router-dom";
 
-import React from "react";
+const Login = ({ isLoggedIn, setIsLoggedIn, setUsername, username }) => {
+  let history = useHistory();
 
+  let [password, setPassword] = useState("");
 
-const Login = () => {
-  return (
-    <div>
-
-      
-    <header>
-      <h1>Christian is doing this page because he is awesome!</h1>
-    </header>
-    <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> 
-    <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/><br/> 
-    </div>
+  let loginForm = (
+    <Container>
+    <Col md={{ span: 4, offset: 1 }}>
+      Login Form
+      <Form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          try {
+            const { data } = await loginUser(username, password);
+            setUsername("");
+            setPassword("");
+            setIsLoggedIn(true);
+          } catch (error) {
+            console.log(error.message);
+          } finally {
+            setIsLoggedIn(true);
+            history.push("/Home");
+          }
+        }}
+      >
+        <Form.Group className="mb-3" controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="usernames"
+            value={username}
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label> enter password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+        </Form.Group>
+        <ButtonToolbar className="mb-2">
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+          <Button
+            variant="secondary"
+            type="submit"
+            onClick={(e) => {
+              history.push("/Register");
+            }}
+          >
+            Don't have an account?
+          </Button>
+        </ButtonToolbar>
+      </Form>
+    </Col>
+    </Container>
   );
+
+  let logoutButton = (
+    <ButtonToolbar className="mb-2">
+      <Button
+        variant="primary"
+        onClick={() => {
+          setIsLoggedIn(false);
+          setUsername("");
+          setPassword("");
+          history.push("/Login");
+        }}
+      >
+        Logout
+      </Button>
+    </ButtonToolbar>
+    
+  );
+  return isLoggedIn ? logoutButton : loginForm;
 };
 
 export default Login;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,19 +1,67 @@
 // Christian is doing register
 
-import React from "react";
-
-const Register = () => {
-  
-
+import React, { useState, useEffect } from "react";
+import { Form, Button, Col, ButtonToolbar } from "react-bootstrap";
+import { getToken, storeToken } from "../auth";
+import { registerUser } from "../api";
+import { useHistory } from "react-router-dom";
+const Register = ({ isLoggedIn, setIsLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  let history = useHistory();
   return (
-    <div>
-      <header>
-    <h1>Please Register Christian</h1>
-  </header>
-  <h1> <br/> <br/> <br/> <br/> <br/> <br/> <br/>  </h1>
-    <h1> <br/> <br/> <br/> <br/> <br/> <br/> <br/>  </h1>
-  </div>
-  )
+    <Container>
+    <Col md={{ span: 4, offset: 1 }}>
+      Register form
+      <Form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          try {
+            const { data } = await registerUser(username, password);
+            setIsLoggedIn(true);
+          } catch (error) {
+            console.log(error.message);
+          } finally {
+            history.push("/Login");
+          }
+        }}
+      >
+        <Form.Group className="mb-3" controlId="formBasicUsername">
+          <Form.Label>Create Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="usernames"
+            value={username}
+            onChange={(event) => {
+              // console.log(event.target.value)
+              console.log(username);
+              setUsername(event.target.value);
+            }}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your username or password with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label> Create Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+        </Form.Group>
+        <ButtonToolbar className="mb-2">
+          <Button variant="primary" type="submit">
+            Register
+          </Button>
+        </ButtonToolbar>
+      </Form>
+    </Col>
+    </Container>
+  );
 };
 
 export default Register;
