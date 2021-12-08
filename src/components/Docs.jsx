cea4091496e6a8d27e5c8c03abd1ffd8d39401f5
mcvollenweider <mcvollenweider@gmail.com>
2021-12-08 12:24:27 -0500

    notes

--- src/components/Docs.jsx
+++ src/components/Docs.jsx
@@ -1,14 +1,51 @@
-import React from 'react'
-
+import React from "react";
+import {
+  Form,
+  Dropdown,
+  DropdownButton,
+  Col,
+  Container,
+} from "react-bootstrap";
 function Docs() {
-    return (
-        <div>
-        <h1>Who is doing this Docs page?</h1>
-<br/> <br/> <br/> <br/> <br/> <br/> <br/>  
-<br/> <br/> <br/> <br/> <br/> <br/> <br/> 
-<br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> 
+  return (
+    <div>
+      <br /> <br /> <br /> <br /> <br />
+      <Container>
+      <Col md={{ span: 4, offset: 4 }}>
+        <h1>FAQS</h1>
+        <br />  
+        <DropdownButton id="dropdown-item-button" title="What is an NFT?">
+          <Dropdown.ItemText>
+            <b> What is an NFT? </b>
+            An NFT is a digital asset stored on the blockchain. These assets
+            hold information, like descriptions, properties, and media files.
+            NFTs are unique, one-of-a-kind, and non-interchangeable. The history
+            of every NFT is recorded on the blockchain, meaning buyers can prove
+            their ownership and creators can receive royalties every time their
+            work is re-sold. NFTs can be traded on marketplaces, proudly
+            displayed in online galleries or used to access exclusive content
+            and real-life experiences.
+          </Dropdown.ItemText>
+        </DropdownButton>
+        <br /> <br /> <br /> <br /> <br />
+        <DropdownButton id="dropdown-item-button" title="How to get started?">
+          <Dropdown.ItemText>
+            <b> How to get started? </b>
+            Currently, we’re most interested in promoting projects that have
+            multi-tiered sales (i.e. 1-of-1s, 1-of-manys, etc), have interesting
+            & unique artwork, include robust digital unlockable content
+            experiences, and have already been marketed in well-trafficked
+            channels. If you fit this description, please fill in our
+            partnership request
+          </Dropdown.ItemText>
+        </DropdownButton>
+        </Col>
+      </Container>
+      <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
     </div>
-    )
+  );
 }
 
 export default Docs;
