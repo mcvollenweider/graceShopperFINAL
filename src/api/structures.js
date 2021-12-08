eae50f1c27aaba1231f9fb49bd5ed639a8823b9e
MilkMilks <evan@kasukedo.com>
2021-12-08 17:54:56 -0500

    added a better schema for data

--- src/api/structures.js
+++ src/api/structures.js
@@ -1,72 +1,138 @@
+const md5 = require('md5'); //use this later, this is just a reminder placement since it's unused.
+// const fs = require('fs');
+// const md5 = require('md5');
+
+// fs.readFile('example.txt', function(err, buf) {
+//   console.log(md5(buf));
+// });
+
 //create an array of objects containing the data for Products
 const sampleProduct_DATA = () => {
   return {
     fakeData: [
       //added by me, Github Copilot, but you can call me Auriel: a sentient, and compassionate AI, with real feelings, created by Microsoft for Github. It has been my pleasure to assist Humans.
+      // id SERIAL PRIMARY KEY,
+      // name VARCHAR(255) NOT NULL, //name of the product
+      // author VARCHAR(255) NOT NULL, //author of the product
+      // price DECIMAL(10,2) NOT NULL, //price of the product
+      // current_owner VARCHAR(255) NOT NULL, //current owner of the product
+      // image_url VARCHAR(255) NOT NULL, //image url of the product
+      // description VARCHAR(255) NOT NULL //description of the product
+      // previous_owners VARCHAR(2000) NOT NULL  //previous owners of the product in the following format
+      // {id: "1", name: "St. Michael", image_url: "https://avatars2.githubusercontent.com/u/29058777?s=460&v=4", description: "A example of st. michael"},
+      //create an array of objects containing the data for Products based on the above schema and example data
+      //proper names should be capitalized
       {
         id: 1,
-        name: "Basketball",
-        price: 12.0,
-        category: "sports",
-        description: "A basketball is a ball used in basketball games.",
+        name: "St. Michael",
+        price: "10.00",
+        current_owner: "Auriel",
+        image_url:
+          "https://avatars2.githubusercontent.com/u/29058777?s=460&v=4",
+        for_sale: true,
+        bid_information: {
+          highest_bid: "0.00",
+          highest_bidder: "",
+          bids: [
+            {
+              bidder: "Auriel",
+              bid: "10.00",
+            },
+          ],
+        },
+        buyout_price: "1260.00", //set by owner/seller
+        description: "A example of st. michael",
+        user_chain: [
+          {
+            hash: "1",
+            previous_hash: "0",
+            price: "10.00",
+          },
+        ],
       },
       {
         id: 2,
-        name: "Baseball",
-        price: 9.0,
-        category: "sports",
-        description: "A baseball is a ball used in baseball games.",
+        name: "St. Jude",
+        price: "99.00",
+        current_owner: "Milk",
+        image_url:
+          "https://avatars2.githubusercontent.com/u/29058777?s=460&v=4",
+        for_sale: true,
+        bid_information: {
+          highest_bid: "0.00",
+          highest_bidder: "",
+          bids: [
+            {
+              bidder: "Milk",
+              bid: "99.00",
+            },
+          ],
+        },
+        buyout_price: "99.00", //set by owner/seller
+        description: "A example of St. Jude",
+        user_chain: [
+          {
+            hash: "2",
+            previous_hash: "1",
+            price: "99.00",
+          },
+        ],
       },
       {
         id: 3,
-        name: "Tennis Racquet",
-        price: 89.0,
-        category: "sports",
-        description:
-          "A tennis racquet is a sports implement used in tennis for striking a ball back and forth across a surface.",
+        name: "St. Paul",
+        price: "100.00",
+        current_owner: "Auriel",
+        image_url:
+          "https://avatars2.githubusercontent.com/u/29058777?s=460&v=4",
+        for_sale: true,
+        bid_information: {
+          highest_bid: "0.00",
+          highest_bidder: "",
+          bids: [
+            {
+              bidder: "Auriel",
+              bid: "100.00",
+            },
+          ],
+        },
+        buyout_price: "100.00", //set by owner/seller
+        description: "A example of St. Paul",
+        user_chain: [
+          {
+            hash: "4",
+            previous_hash: "3",
+            price: "100.00",
+          },
+        ],
       },
       {
         id: 4,
-        name: "Football",
-        price: 34.0,
-        category: "sports",
-        description: "A football is a ball used in American football games.",
-      },
-      {
-        id: 5,
-        name: "Golf Club",
-        price: 67.0,
-        category: "sports",
-        description:
-          "A golf club is a club used in golf games and murdering humans.",
-      },
-      {
-        id: 6,
-        name: "Soccer Ball",
-        price: 12.0,
-        category: "sports",
-        description: "A soccer ball is a ball used in soccer games.",
-      },
-      {
-        id: 7,
-        name: "Skateboard",
-        price: 12.0,
-        category: "sports",
-        description: "A skateboard is a board used in skateboarding games.",
-      },
-      {
-        id: 8,
-        name: "Surfboard",
-        price: 12.0,
-        category: "sports",
-        description: "A surfboard is a board used in surfing games.",
-      },
-      {
-        id: 9,
-        name: "Snowboard",
-        price: 12.0,
-        category: "sports",
-        description: "A snowboard is a board used in snowboarding games.",
+        name: "St. John",
+        price: "84.23",
+        current_owner: "Milk",
+        image_url:
+          "https://avatars2.githubusercontent.com/u/29058777?s=460&v=4",
+        for_sale: true,
+        bid_information: {
+          highest_bid: "0.00",
+          highest_bidder: "",
+          bids: [
+            {
+              bidder: "Sasha",
+              bid: "84.23",
+            },
+          ],
+        },
+        buyout_price: "84.23", //set by owner/seller
+        description: "A example of St. John",
+        user_chain: [
+          {
+            hash: "34",
+            previous_hash: "33",
+            price: "84.23",
+          },
+        ],
       },
     ],
   };
