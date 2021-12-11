953a32d4395a881d1bddbf5f1ddfc49f1a718811
Daniel Rivera <85755386+Driver-glitch@users.noreply.github.com>
2021-12-11 15:56:38 -0500

    added background to settings

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -3,11 +3,29 @@ import React from 'react'
 function Settings() {
     return (
         <div>
-        <header>
-      <h1> Who is doing the Settings page? </h1>
-    </header>
-    <h1> <br/> <br/> <br/> <br/> <br/> <br/> <br/>  </h1>
-      <h1> <br/> <br/> <br/> <br/> <br/> <br/> <br/>  </h1>
+        <div
+      className="bg-image "
+      style={{
+        backgroundImage:
+          "url('https://i2.wp.com/files.123freevectors.com/wp-content/original/115982-abstract-cool-blue-blurred-lights-background-design.jpg?w=800&q=95)",
+        height: "100vh",
+        backgroundSize: "100%",
+      }}
+    >
+      <div
+        class="mask"
+        style={{
+          backgroundColor: "rgba(0, 0, 0, 0.6)",
+          height: "100vh",
+          backgroundSize: "100%",
+        }}
+      >
+
+{/* Code goes here */}
+
+
+      </div>
+      </div>
     </div>
     )
 }
