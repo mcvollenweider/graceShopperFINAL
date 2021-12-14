7683ac17a0c6852b4d86776ae6c460d36d543626
mcvollenweider <mcvollenweider@gmail.com>
2021-12-13 19:29:47 -0500

    added About photo and small bio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -64,19 +64,11 @@ function About() {
                   }}
                 />
                 <p>
-                  Contrary to popular belief, Lorem Ipsum is not simply random
-                  text. It has roots in a piece of classical Latin literature
-                  from 45 BC, making it over 2000 years old. Richard McClintock,
-                  a Latin professor at Hampden-Sydney College in Virginia,
-                  looked up one of the more obscure Latin words, consectetur,
-                  from a Lorem Ipsum passage, and going through the cites of the
-                  word in classical literature, discovered the undoubtable
-                  source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of
-                  "de Finibus Bonorum et Malorum" (The Extremes of Good and
-                  Evil) by Cicero, written in 45 BC. This book is a treatise on
-                  the theory of ethics, very popular during the Renaissance. The
-                  first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..",
-                  comes from a line in section 1.10.32.
+                  Christian Vollenweider is from Jacksonville Florida. He
+                  recently decided to pursue a career change and joined the
+                  Fullstack Academy 2021 Cohort. Christian is graduating
+                  from Fullstack with a web development certificate and hopes to
+                  enter the tech field as a Front-End Developer soon.
                 </p>
               </div>
             </div>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -32,42 +32,37 @@ const Profile = () => {
   }, []);
   console.log(allProducts);
   return (
-    
-         <div className="bg-image">
+    <div className="bg-image">
       <div className="mask"></div>
       <Container>
-        <Card className="text-center">
-          <Card.Header>User Profile</Card.Header>
-          <Card.Body>
-            <Card.Title>{userName.toUpperCase()}</Card.Title>
-            <Card.Text>Browse through your NFTs below</Card.Text>
-          </Card.Body>
-          {/* <Card.Footer className="text-muted">2 days ago</Card.Footer> */}
-        </Card>
+        <h1 className="text-center text-white p-4 mt-0 mb-1 mb-md-0">
+          {userName.toUpperCase()}'s Profile Page
+        </h1>
+        <h3 className="text-center text-white p-4 mt-0 mb-1 mb-md-0">Your NFTs</h3>
         <Row className="m-5">
           {allProducts && allProducts.length
             ? allProducts.map((product, index) => {
                 //console.log(product);
-                if (userName === product.current_owner){
-                return (
-                  <Card
-                    className="m-1"
-                    key={product.id + index}
-                    style={{ width: "18rem" }}
-                  >
-                    <Card.Img className="mt-3" src={product.image_url} />
-                    <Card.Body className="text-center">
-                      <Card.Title>{product.name}</Card.Title>
-                      <Card.Text>{product.description}</Card.Text>
-                      <Card.Text>{product.current_owner}</Card.Text>
-                    </Card.Body>
-                  </Card>
-                );}
+                if (userName === product.current_owner) {
+                  return (
+                    <Card
+                      className="m-1"
+                      key={product.id + index}
+                      style={{ width: "18rem" }}
+                    >
+                      <Card.Img className="mt-3" src={product.image_url} />
+                      <Card.Body className="text-center">
+                        <Card.Title>{product.name}</Card.Title>
+                        <Card.Text>{product.description}</Card.Text>
+                        <Card.Text>{product.current_owner}</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  );
+                }
               })
             : "No products found"}
         </Row>
       </Container>
-     
       <br /> <br /> <br /> <br /> <br /> <br /> <br />
       <br /> <br /> <br /> <br /> <br /> <br /> <br />
       <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
